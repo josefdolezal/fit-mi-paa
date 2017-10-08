cb915c9ec692e3015e3351bb51b9709c2d062e5c
Josef Dolezal <pepik.ml@gmail.com>
2017-10-08 18:14:41 +0200

    Create model object for backpack

---
+++ mi-paa-backpack-problem/mi-paa-backpack-problem/Model/BackpackItem.swift
@@ -0,0 +1,19 @@
+//
+//  BackpackItem.swift
+//  mi-paa-backpack-problem
+//
+//  Created by Josef Dolezal on 08/10/2017.
+//  Copyright © 2017 Josef Dolezal. All rights reserved.
+//
+
+import Foundation
+
+class BackpackItem {
+    let value: Int
+    let weight: Int
+
+    init(value: Int, weight: Int) {
+        self.value = value
+        self.weight = weight
+    }
+}
