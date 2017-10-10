24c69a58e8c544f947ec3313e5e7879d753d0f17
Josef Dolezal <pepik.ml@gmail.com>
2017-10-10 22:29:27 +0200

    Add BestRation fitting strategy

--- mi-paa-backpack-problem/mi-paa-backpack-problem/Utility/BackpackFittingStrategy.swift
+++ mi-paa-backpack-problem/mi-paa-backpack-problem/Utility/BackpackFittingStrategy.swift
@@ -28,3 +28,20 @@ class AllCasesBackpackFittingStrategy: BackpackFittingStrategyType {
         }
     }
 }
+
+class BestRatioBackpackFittingStrategy: BackpackFittingStrategyType {
+
+    static func fit(items: [BackpackItem], maxWeight: Int) -> BackpackFittingResult? {
+        return items.map { ($0.value, $0.weight, Double($0.value) / Double($0.weight)) }
+            .sorted { lhs, rhs in lhs.2 >= rhs.2 }
+            .reduce((0, 0) as BackpackFittingResult,
+                    while: { $0.weight + $1.1 <= maxWeight }) {
+
+                let newWeight = $0.weight + $1.1
+
+                guard newWeight <= maxWeight else { return $0 }
+
+                return BackpackFittingResult(weight: newWeight, value: $0.value + $1.0)
+            }
+    }
+}
--- mi-paa-backpack-problem/mi-paa-backpack-problemTests/Utility/BackPackFittingStrategyTests.swift
+++ mi-paa-backpack-problem/mi-paa-backpack-problemTests/Utility/BackPackFittingStrategyTests.swift
@@ -10,20 +10,29 @@ import XCTest
 
 class BackPackFittingStrategyTests: XCTestCase {
 
+    var backpack: Backpack!
     var backpackItems: [BackpackItem]!
 
     override func setUp() {
         super.setUp()
 
+        backpack = Backpack(maxWeight: 49)
         backpackItems = [(10, 15), (7, 11), (19, 2), (9, 3), (1, 20), (20, 1), (2, 4), (8, 8)]
             .map { BackpackItem(value: $0.1, weight: $0.0) }
     }
 
     func testAllCasesBackpackFittingStrategy() {
-        let backpack = Backpack(maxWeight: 50)
         let subject = backpack.fit(items: backpackItems, using: AllCasesBackpackFittingStrategy.self)
 
         XCTAssertEqual(subject.value, 61)
+        XCTAssertEqual(subject.weight, 37)
+    }
+
+    func testBestRationFittingStrategy() {
+        let subject = backpack.fit(items: backpackItems, using: BestRatioBackpackFittingStrategy.self)
+
+        XCTAssertEqual(subject.value, 61)
+        XCTAssertEqual(subject.weight, 37)
     }
 
 }
