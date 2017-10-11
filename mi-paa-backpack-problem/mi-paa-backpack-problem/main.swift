883a854357c1faad182040cc3cbe4b13fcacac03
Josef Dolezal <pepik.ml@gmail.com>
2017-10-11 23:21:38 +0200

    Implement simple problem solver as main func

--- mi-paa-backpack-problem/mi-paa-backpack-problem/main.swift
+++ mi-paa-backpack-problem/mi-paa-backpack-problem/main.swift
@@ -10,5 +10,24 @@ import Foundation
 
 let filesReader = BackpackInstanceReader(fileManager: FileManager.default)
 
-let instances = try! filesReader.readFiles(at: URL(string: "./Input")!, ofType: BackpackProblemInstance.self)
-let solutions = try! filesReader.readFiles(at: URL(string: "Output")!, ofType: BackpackProblemSolution.self)
+let instanceGroups = try! filesReader.readFiles(at: URL(string: "./Input")!, ofType: BackpackProblemInstance.self)
+let solutionGroups = try! filesReader.readFiles(at: URL(string: "./Output")!, ofType: BackpackProblemSolution.self)
+
+for group in zip(instanceGroups, solutionGroups) {
+    let (instances, solutions) = group
+
+    for configuration in zip(instances, solutions) {
+        let (instance, solution) = configuration
+        let backpack = Backpack(maxWeight: instance.backpackMaxWeight)
+        let start = CFAbsoluteTimeGetCurrent()
+
+        let fittingResult = backpack.fit(items: instance.backpackItems, using: AllCasesBackpackFittingStrategy.self)
+        let duration = CFAbsoluteTimeGetCurrent() - start
+
+        if fittingResult.value == solution.backpackValue {
+            print("✅ Test \(instance.id) passed in \(duration)")
+        } else {
+            print("🚫 Test \(instance.id) failed in \(duration)")
+        }
+    }
+}
