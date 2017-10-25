8635d47802e9057d8ba881fa7452052aebc9f2b1
Josef Dolezal <pepik.ml@gmail.com>
2017-10-25 20:56:40 +0200

    Create model for test result

---
+++ backpack-problem/Sources/BackpackBenchmark/TestResult.swift
@@ -0,0 +1,15 @@
+//
+//  TestResult.swift
+//  BackpackBenchmark
+//
+//  Created by Josef Dolezal on 22/10/2017.
+//
+
+import Foundation
+
+public struct TestResult {
+    public let allCasesDuration: Double
+    public let approximationDuration: Double
+
+    public let approxamationError: Double
+}
