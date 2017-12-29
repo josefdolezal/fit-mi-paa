806dc1dfe74dcac406dacae182f9ee09e70c09cc
Josef Dolezal <pepik.ml@gmail.com>
2017-12-29 14:09:32 +0100

    Add random solution generator for simulated annealing

--- backpack-problem/Sources/BackpackSolver/Utility/SimulatedAnnealingFittingStrategy.swift
+++ backpack-problem/Sources/BackpackSolver/Utility/SimulatedAnnealingFittingStrategy.swift
@@ -7,6 +7,26 @@
 
 import Foundation
 
+class SimulatedAnnealingFittingStrategy: BackpackFittingStrategyType {
+    static func fit(items: [BackpackItem], maxWeight: Int) -> BackpackFittingResult? {
+        return nil
+    }
+
+    private static func randomSolution(for items: [BackpackItem], maxWeight: Int) -> BackpackSolutionSkeleton {
+        let randomSkeleton = items.map { _ in arc4random() % 2 == 1 }
+        let (value, weight) = zip(items, randomSkeleton)
+            .filter { items, included in included }
+            .map { item, _ in item }
+            .reduce((0, 0)) { intermediate, item in (intermediate.0 + item.value, intermediate.1 + item.weight) }
+
+        if weight <= maxWeight {
+            return BackpackSolutionSkeleton(value: value, weight: weight, items: items, skeleton: randomSkeleton)
+        }
+
+        return randomSolution(for: items, maxWeight: maxWeight)
+    }
+}
+
 struct BackpackSolutionSkeleton {
     let value: Int
     let weight: Int
