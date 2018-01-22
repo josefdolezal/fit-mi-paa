f7c18e27b672471f4ffc32347ee6740c757b66ad
Josef Dolezal <pepik.ml@gmail.com>
2018-01-22 23:10:10 +0100

    Create sat assignment project structure

---
+++ three-sat/ThreeSAT/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ThreeSAT
+//
+//  Created by Josef Dolezal on 22/01/2018.
+//  Copyright © 2018 Josef Dolezal. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
