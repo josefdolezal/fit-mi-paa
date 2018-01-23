6b59f4994ea4b8fb91f4e92ce7f5e222312f130d
Josef Dolezal <pepik.ml@gmail.com>
2018-01-23 23:57:54 +0100

    Use SPM for project structure management

---
+++ three-sat/Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version:4.0
+
+import PackageDescription
+
+let package = Package(
+    name: "three-sat",
+    dependencies: [],
+    targets: [
+        .target(
+            name: "ThreeSAT",
+            dependencies: []),
+        .testTarget(
+            name: "ThreeSATTests"
+        )
+    ]
+)
